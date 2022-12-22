28b7c7f94f1262303fd0d3d27034cdfeba9eb8f9
denys_kosyriev <utross2241@gmail.com>
2022-12-22 18:17:40 +0200

    fix 1

--- index.js
+++ index.js
@@ -3,25 +3,29 @@ let input = document.getElementsByClassName('input')[0];
 let list = document.getElementsByClassName('list')[0];
 input.addEventListener('input', (e) => {
   let inputValue = e.target.value;
-  let passwordValidationConditions = [];
+  let isValid = [];
   let num = 0;
-  passwordValidationConditions.push(/.{8}/.test(inputValue));
-  passwordValidationConditions.push(/\d/.test(inputValue));
-  passwordValidationConditions.push(/[A-Z]/.test(inputValue));
-  passwordValidationConditions.push(/[a-z]/.test(inputValue));
-  passwordValidationConditions.push(/\W/.test(inputValue));
-  input.classList.add('input-active');
-  passwordValidationConditions.forEach((el, id) => {
+  isValid.push(/.{8}/.test(inputValue));
+  isValid.push(/\d/.test(inputValue));
+  isValid.push(/[A-Z]/.test(inputValue));
+  isValid.push(/[a-z]/.test(inputValue));
+  isValid.push(/\W/.test(inputValue));
+  input.classList.add('input-active')
+  if (inputValue.length === 0) input.classList.add('input-empty');
+  else input.classList.remove('input-empty');
+  isValid.forEach((el, id) => {
     if (el) {
       item[id].classList.add('item-valid');
       num++;
-      console.log(num)
     } else {
       item[id].classList.remove('item-valid');
     }
     if (num === item.length) {
       input.classList.add('input-valid')
       list.classList.add('list-valid')
+    } else {
+      input.classList.remove('input-valid');
+      list.classList.remove('list-valid');
     }
   })
 })
